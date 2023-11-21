7242b24ca967d2d62a38f8fd2310cdf646b7e525
Carlos Felgueiras <catofel2000@gmail.com>
2023-11-21 10:25:11 +0000

    moved default hostname and port to config file, renamed ClientState to Client

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -4,14 +4,13 @@
 #include "protocol.hpp"
 #include <string>
 
-class ClientState {
+class Client {
   private:
-    std::string _hostname =
-        "127.0.0.1"; // Perguntar se pode ser localhost ou tem de ser 127.0.0.1
-    std::string _port = "58000";
+    std::string _hostname = DEFAULT_HOSTNAME; // Perguntar se pode ser localhost ou tem de ser 127.0.0.1
+    std::string _port = DEFAULT_PORT;
 
   public:
-    ClientState(int argc, char **argv);
+    Client(int argc, char **argv);
     void ShowInfo();
 };
 
--- src/common/config.hpp
+++ src/common/config.hpp
@@ -6,4 +6,7 @@
 #define PROTOCOL_PASSWORD_SIZE (8)
 #define PROTOCOL_AID_SIZE (3)
 
+#define DEFAULT_HOSTNAME "127.0.0.1"
+#define DEFAULT_PORT "58085"
+
 #endif
