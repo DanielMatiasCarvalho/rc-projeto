467dca7aa782b8265a1f0f0ce2d4423b28e1931c
DanielMatiasCarvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2023-11-21 23:03:51 +0000

    login, logout, unregister and exit command all done, except handling exceptions

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -5,13 +5,27 @@
 #include "protocol.hpp"
 #include <string>
 
+class User {
+  private:
+    std::string _username;
+    std::string _password;
+  
+  public:
+    bool isLoggedIn();
+    void logIn(std::string username, std::string password);
+    void logOut();
+    std::string getUsername();
+    std::string getPassword();
+};
+
 class Client {
   private:
-    std::string _hostname = DEFAULT_HOSTNAME; // Perguntar se pode ser localhost
-                                              // ou tem de ser 127.0.0.1
+    std::string _hostname = DEFAULT_HOSTNAME; 
     std::string _port = DEFAULT_PORT;
 
   public:
+    User _user;
+    bool _toExit = false;
     Client(int argc, char **argv);
     void ShowInfo();
     void processRequest(ProtocolCommunication &comm);
