451c33a7a4836da33238348f3503d63b8a8f67ac
DanielMatiasCarvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2023-12-01 23:11:53 +0000

    Comments done

--- src/server/command.hpp
+++ src/server/command.hpp
@@ -10,27 +10,55 @@
 #include "protocol.hpp"
 #include "server.hpp"
 
+/**
+ * @brief The base class for command handlers.
+ * 
+ * This class provides an interface for handling commands and defines a common member variable.
+ */
 class CommandHandler {
   public:
+    /**
+     * @brief Handles the command.
+     * 
+     * This pure virtual function must be implemented by derived classes to handle the command.
+     * 
+     * @param message The command message.
+     * @param receiver The server object that receives the command.
+     */
     virtual void handle(std::stringstream &message, Server &receiver) = 0;
 
-    std::string _code;
-    std::string _description;
-    std::string _usage;
+    std::string _code; /**< The code associated with the command. */
 
   protected:
-    CommandHandler(std::string code, std::string description, std::string usage)
-        : _code{code}, _description{description}, _usage{usage} {}
+    /**
+     * @brief Constructs a CommandHandler object.
+     * 
+     * @param code The code associated with the command.
+     */
+    CommandHandler(std::string code)
+        : _code{code} {}
 };
 
+/**
+ * @brief The CommandManager class handles the registration and execution of command handlers.
+ */
 class CommandManager {
   private:
     std::unordered_map<std::string, std::shared_ptr<CommandHandler>> _handlers =
         {};
 
   public:
+    /**
+     * @brief Registers a command handler.
+     * @param handler The command handler to register.
+     */
     void registerHandler(std::shared_ptr<CommandHandler> handler);
 
+    /**
+     * @brief Reads and executes a command from the given message.
+     * @param message The message containing the command.
+     * @param receiver The server object that will receive the command.
+     */
     void readCommand(std::stringstream message, Server &receiver);
 };
 
--- src/server/server.hpp
+++ src/server/server.hpp
@@ -6,20 +6,34 @@
 
 #include "config.hpp"
 
+/**
+ * @class Server
+ * @brief Represents a server object.
+ */
+
 class Server {
-  private:
-    std::string _port = DEFAULT_PORT;
-    bool _verbose = false;
+    private:
+        std::string _port = DEFAULT_PORT; /**< The port number. */
+        bool _verbose = false; /**< Flag indicating whether to display verbose output. */
 
-  public:
-    Server(int argc, char **argv);
+    public:
+        /**
+         * @brief Constructs a Server object.
+         * @param argc The number of command-line arguments.
+         * @param argv The command-line arguments.
+         */
+        Server(int argc, char **argv);
 
-    /**
-     * @brief Displays information about the server.
-     */
-    void ShowInfo();
+        /**
+         * @brief Displays information about the server.
+         */
+        void ShowInfo();
 
-    void showMessage(std::string message);
+        /**
+         * @brief Displays a message.
+         * @param message The message to display.
+         */
+        void showMessage(std::string message);
 };
 
 #endif
