54ab07cc1d3ca2c006ad0e1b516c819356c377f3
DanielMatiasCarvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2023-12-01 22:53:59 +0000

    Server constructor, show info and show message implemented

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,7 +1,53 @@
-#include <iostream>
+#include "server.hpp"
 
 int main() {
     std::cout << "Not implemented" << std::endl;
 
     return 1;
+}
+
+/**
+ * @brief Constructs a Server object.
+ * 
+ * This constructor initializes the Server object with the provided command line arguments.
+ * It parses the command line arguments and sets the port and verbosity options accordingly.
+ * 
+ * @param argc The number of command line arguments.
+ * @param argv An array of command line arguments.
+ */
+Server::Server(int argc, char **argv) {
+    char c;
+
+    while ((c = (char)getopt(argc, argv, "p:v")) != -1) {
+        switch (c) {
+            case 'p':
+                _port = optarg;
+                break;
+            case 'v':
+                _verbose = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+/**
+ * @brief Displays the information about the server.
+ * 
+ * This function prints the port number and verbosity level of the server.
+ */
+void Server::ShowInfo() {
+    std::cout << "Port: " << _port << std::endl
+              << "Verbose: " << _verbose << std::endl;
+}
+
+/**
+ * Displays a message if the server is in verbose mode.
+ *
+ * @param message The message to be displayed.
+ */
+void Server::showMessage(std::string message) {
+    if (_verbose)
+        std::cout << message << std::endl;
 }
--- src/server/server.hpp
+++ src/server/server.hpp
@@ -1,6 +1,25 @@
 #ifndef __CLIENT_HPP__
 #define __CLIENT_HPP__
 
-class Server {};
+#include <unistd.h>
+#include <iostream>
+
+#include "config.hpp"
+
+class Server {
+  private:
+    std::string _port = DEFAULT_PORT;
+    bool _verbose = false;
+
+  public:
+    Server(int argc, char **argv);
+
+    /**
+     * @brief Displays information about the server.
+     */
+    void ShowInfo();
+
+    void showMessage(std::string message);
+};
 
 #endif
