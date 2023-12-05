3f5ab96be5c581ad1fdbf252af0f4b10161c3e83
Carlos Felgueiras <catofel2000@gmail.com>
2023-12-05 23:40:53 +0000

    fmt

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -21,7 +21,7 @@ class Server {
     bool _verbose =
         false; /**< Flag indicating whether to display verbose output. */
   public:
-     std::unique_ptr<Database> _database; /**< The database. */
+    std::unique_ptr<Database> _database; /**< The database. */
     /**
          * @brief Constructs a Server object.
          * @param argc The number of command-line arguments.
