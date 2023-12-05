15732f1a40b3ea25d7f59d4a463dcb4cf6d6ddc7
DanielMatiasCarvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2023-12-05 14:33:27 +0000

    udp max datagram size updated

--- src/common/network.cpp
+++ src/common/network.cpp
@@ -50,7 +50,7 @@ void UdpClient::send(std::stringstream &message) {
 }
 
 std::stringstream UdpClient::receive() {
-    char messageBuffer[SOCKETS_MAX_DATAGRAM_SIZE];
+    char messageBuffer[SOCKETS_MAX_DATAGRAM_SIZE+1];
     socklen_t addrlen = sizeof(_addr);
     ssize_t n = recvfrom(_fd, messageBuffer, SOCKETS_MAX_DATAGRAM_SIZE + 1, 0,
                          (struct sockaddr *)&_addr, &addrlen);
