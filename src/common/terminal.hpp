0408bb75073654b41226c2dc5744f7231c231fb4
Carlos Felgueiras <catofel2000@gmail.com>
2023-12-07 23:21:10 +0000

    fowmwat

--- src/common/terminal.hpp
+++ src/common/terminal.hpp
@@ -1,14 +1,14 @@
 #ifndef __TERMINAL_HPP__
 #define __TERMINAL_HPP__
 
-#include <string>
-#include <stdexcept>
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
+#include <stdexcept>
+#include <string>
 
 class Terminal {
-    public:
-        std::string readLine(std::string prompt);
+  public:
+    std::string readLine(std::string prompt);
 };
 
 #endif
